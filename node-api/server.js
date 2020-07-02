a2738abea05e04653a2a9648dae8b07c9c9cd4f8
Simone Lopes da Silva <simlsilva@firjan.com.br>
2020-07-02 20:52:43 -0300

    Configuração de conexão com mongoDb

--- node-api/server.js
+++ node-api/server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+//iniciando o app
 const app = express();
 
+
+//iniciando o DB
+mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true});
+
+//primeira rota
 app.get('/', (req, res)=>{
    res.send('Hello Rocketseat');
 });

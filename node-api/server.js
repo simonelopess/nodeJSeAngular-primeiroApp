5a91cc96372a7c30deb50fbdf4c065e8bc113720
Simone Lopes da Silva <simlsilva@firjan.com.br>
2020-07-02 22:24:09 -0300

    Restruturação de arquivos

--- node-api/server.js
+++ node-api/server.js
@@ -12,18 +12,8 @@ mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true});
 requireDir('./src/models');
 
 
-const Product = mongoose.model('Product');
+//Rotas
+app.use('/api', require('./src/routes'));
 
-//primeira rota
-app.get('/', (req, res)=>{
-   Product.create(
-      {
-         title:'React Native',
-         description: 'Build native apps with React',
-         url: 'http://github.com/facebook/react-native'
-      }
-   )
-   return res.send('Hello Rocketseat');
-});
 
 app.listen(3001);

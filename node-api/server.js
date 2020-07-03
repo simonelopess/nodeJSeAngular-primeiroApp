5773b6ad0b2fdba5545f46b140db049b35a7d139
Simone Lopes da Silva <simlsilva@firjan.com.br>
2020-07-02 22:01:11 -0300

    Criando model Product

--- node-api/server.js
+++ node-api/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 //iniciando o app
 const app = express();
@@ -8,9 +9,21 @@ const app = express();
 //iniciando o DB
 mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true});
 
+requireDir('./src/models');
+
+
+const Product = mongoose.model('Product');
+
 //primeira rota
 app.get('/', (req, res)=>{
-   res.send('Hello Rocketseat');
+   Product.create(
+      {
+         title:'React Native',
+         description: 'Build native apps with React',
+         url: 'http://github.com/facebook/react-native'
+      }
+   )
+   return res.send('Hello Rocketseat');
 });
 
 app.listen(3001);

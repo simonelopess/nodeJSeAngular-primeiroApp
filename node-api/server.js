1ce1af97a7a88b69531fd15ae918c0db57ce0af1
Simone Lopes da Silva <simlsilva@firjan.com.br>
2020-07-02 22:47:20 -0300

    Criação de registro no BD

--- node-api/server.js
+++ node-api/server.js
@@ -4,7 +4,7 @@ const requireDir = require('require-dir');
 
 //iniciando o app
 const app = express();
-
+app.use(express.json());
 
 //iniciando o DB
 mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true});
--- node-api/src/routes.js
+++ node-api/src/routes.js
@@ -4,6 +4,6 @@ const routes = express.Router();
 const ProductController = require('./controllers/ProductController')
 //primeira rota
 routes.get('/products', ProductController.index);
-  
+routes.post('/products', ProductController.store);
 
  module.exports = routes;
